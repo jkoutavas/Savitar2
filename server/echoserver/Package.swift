577891c5724cac3ec4256330f1a11b710897caa6
Jay Koutavas <jay@heynow.com>
2018-02-11 20:03:01 -0500

    Initial commit for the test echo server

---
+++ server/echoserver/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "echoserver",
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMajor(from: "0.12.90")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "echoserver",
+            dependencies: ["Socket"]),
+    ]
+)
---
+++ server/echoserver/README.md
@@ -0,0 +1,3 @@
+# echoserver
+
+A description of this package.
