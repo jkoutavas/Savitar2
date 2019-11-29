03b97a3ca792f2f0c25f565b2aea529f0e475817
Jay Koutavas <jay@heynow.com>
2019-11-28 23:06:21 -0500

    Introducing "Aha" -- an ANSII escape code - to - HTML module, notably hacked-up by me as a PoC

---
+++ client/Savitar2-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Savitar2-Bridging-Header.h
+//  Savitar2
+//
+//  Created by Jay Koutavas on 11/25/19.
+//  Copyright © 2019 Heynow Software. All rights reserved.
+//
+
+#ifndef Savitar2_Bridging_Header_h
+#define Savitar2_Bridging_Header_h
+
+#include "Aha.h"
+
+#endif /* Savitar2_Bridging_Header_h */
---
+++ client/Savitar2/src/thirdparty/aha/aha.h
@@ -0,0 +1,14 @@
+//
+//  aha.h
+//  Savitar2
+//
+//  Created by Jay Koutavas on 9/21/19.
+//  Copyright © 2019 Heynow Software. All rights reserved.
+//
+
+#ifndef aha_h
+#define aha_h
+
+extern int ahamain(int argc,char* args[], char *input, char **output);
+
+#endif /* aha_h */
