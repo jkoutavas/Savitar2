181d6dd896770ac5a04512972459b05a38fc1429
Jay Koutavas <jay@heynow.com>
2019-11-30 18:37:28 -0500

    Added Data+Extensions

---
+++ client/Savitar2/src/extensions/Data+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Data+Extensions.swift
+//  Savitar2
+//
+//  Created by Jay Koutavas on 11/30/19.
+//  Copyright © 2019 Heynow Software. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    mutating func append(string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+  
+    var hexString: String {
+        return self.reduce("", { $0 + String(format: "%02x, ", $1) })
+    }
+}
