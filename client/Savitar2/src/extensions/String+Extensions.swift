1a8da087f8cdd2dccefc151fc84b62e22fcf5f96
Jay Koutavas <jay@heynow.com>
2019-12-03 20:00:28 -0500

    Introducing the "ranges" String extension function

--- client/Savitar2/src/extensions/String+Extensions.swift
+++ client/Savitar2/src/extensions/String+Extensions.swift
@@ -24,4 +24,22 @@ extension String {
         guard self.hasPrefix(prefix) else { return self }
         return String(self.dropFirst(prefix.count))
     }
+
+    func ranges(of occurrence: String, options mask: String.CompareOptions = []) -> [Range<String.Index>] {
+        var ranges = [Range<String.Index>]()
+        var position = startIndex
+        while let range = range(of: occurrence, options: mask, range: position..<endIndex) {
+            ranges.append(range)
+            let offset = distance(from: range.lowerBound,
+                                             to: range.upperBound) - 1
+            guard let after = index(range.lowerBound,
+                                    offsetBy: offset,
+                                    limitedBy: endIndex) else {
+                                        break
+            }
+            position = index(after: after)
+        }
+        return ranges
+    }
+
 }
